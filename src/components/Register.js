a6f814d1a8fd7709ae06c86a7503c405e11eeffc
raythurman2386 <raymondthurman5@gmail.com>
2019-09-11 22:07:10 -0400

    imported hooks

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Form, Field, withFormik } from 'formik'
 import * as Yup from 'yup'
 import axios from 'axios'
