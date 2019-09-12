c29bf652aa005496093e2a52bde15f5982097100
raythurman2386 <raymondthurman5@gmail.com>
2019-09-11 22:43:05 -0400

    app component initial styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
 
+// Styled Components
+import styled from 'styled-components'
+
 // import Register
 import Register from './components/Register'
 
 function App() {
   return (
-    <div className='App'>
+    <AppWrapper>
       <Register />
-    </div>
+    </AppWrapper>
   )
 }
 
 export default App
+
+const AppWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  margin: auto;
+  color: #fff;
+  background-color: #333;
+`
