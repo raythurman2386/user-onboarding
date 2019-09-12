c001d04a293ff15d3253055e9cf5d375c7c7808e
raythurman2386 <raymondthurman5@gmail.com>
2019-09-11 21:35:12 -0400

    form coded out

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Form, Field, withFormik } from 'formik'
 import * as Yup from 'yup'
+import axios from 'axios'
 
 const Register = () => {
   return (
@@ -8,8 +9,13 @@ const Register = () => {
       <Field type='text' name='name' placeholder='Name' />
       <Field type='email' name='email' placeholder='Email' />
       <Field type='password' name='password' placeholder='Password' />
+      <label>
+        <Field type='checkbox' name='tos' />
+        <span>Terms of Service</span>
+      </label>
+      <button type='submit'>Submit</button>
     </Form>
   )
 }
 
-export default Register
+export default withFormik({})(Register)
