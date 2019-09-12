e8d4e1aa07e37fe61080fb29b49ce85a8a5490c4
raythurman2386 <raymondthurman5@gmail.com>
2019-09-11 21:29:50 -0400

    added first three fields

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,13 @@ import { Form, Field, withFormik } from 'formik'
 import * as Yup from 'yup'
 
 const Register = () => {
-  return <div></div>
+  return (
+    <Form>
+      <Field type='text' name='name' placeholder='Name' />
+      <Field type='email' name='email' placeholder='Email' />
+      <Field type='password' name='password' placeholder='Password' />
+    </Form>
+  )
 }
 
 export default Register
